a7eb05e0c9565ca4df7cd2def23d089cfe0bb9d5
Aditya Bhattacharya <69953590+AdityaBhattacharya1@users.noreply.github.com>
2023-09-18 14:32:33 +0530

    add github workflow for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 
-import partytown from "@astrojs/partytown";
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown()]
-});
+	integrations: [react(), partytown()],
+	site: 'adityabhattacharya.github.io',
+})
