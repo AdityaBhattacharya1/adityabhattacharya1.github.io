8241bb388c9258ff74558130c80556f5a3908a2d
Aditya Bhattacharya <69953590+AdityaBhattacharya1@users.noreply.github.com>
2023-09-19 10:37:07 +0530

    update domain names

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import partytown from '@astrojs/partytown';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import partytown from '@astrojs/partytown'
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown(), sitemap()],
-  site: 'https://adityabhattacharya.github.io'
-});
+	integrations: [react(), partytown(), sitemap()],
+	site: 'https://adityabh.is-a.dev',
+})
