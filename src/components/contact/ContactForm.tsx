55381b957e8fef9b9b6efe190cba8b7ebfb35544
Aditya Bhattacharya <69953590+AdityaBhattacharya1@users.noreply.github.com>
2023-11-12 12:01:37 +0530

    github actions my beloved 6

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -5,7 +5,8 @@ import ContactIcon from '../../assets/ContactIcon'
 import useInput from '../../hooks/useForm'
 
 export default function ContactForm() {
-	const [state, handleSubmit] = useForm(import.meta.env.FORMSPREE_KEY)
+	console.log(import.meta.env.VITE_FORMSPREE_KEY)
+	const [state, handleSubmit] = useForm(import.meta.env.PUBLIC_FORMSPREE_KEY)
 	const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/gi
 
 	// form validation fun stuff!
